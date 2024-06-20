53180fc725b35eba60b2f7c09e2745cceefc4cb9
Ruslan Feduleyev <8reflections@gmail.com>
2024-06-20 15:42:24 +0500

    Hash router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { Game1 } from "../pages/game1/game1";
 import { Home } from "../pages/home/Home";
 import { Page } from "../ui/Page/Page";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <Page dataPage={"game1"} children={<Game1 />} />,
