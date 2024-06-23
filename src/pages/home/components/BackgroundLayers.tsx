0ef891dc7f227b8052229d2b9419668e3e445610
Ruslan Feduleyev <8reflections@gmail.com>
2024-06-23 18:05:36 +0500

    Autoplay fix

--- src/pages/home/components/BackgroundLayers.tsx
+++ src/pages/home/components/BackgroundLayers.tsx
@@ -6,7 +6,7 @@ export function BackgroundLayers() {
 
       <iframe
         className="backgroundVideo"
-        src="https://www.youtube.com/embed/j2uk88ZaaGs?autoplay=1&mute=1&loop=1&controls=0&showinfo=0"
+        src="https://www.youtube.com/embed/j2uk88ZaaGs?autoplay=1&mute=1&loop=1&controls=0&showinfo=0&playlist=j2uk88ZaaGs"
         title="4K Space Video Background, Galaxy Video Background. Free Video Background."
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
