1111a647247b5ef9b902ca616a6e37a7152d1c51
Ruslan Feduleyev <8reflections@gmail.com>
2024-09-30 22:07:48 +0500

    Avatar fetch

--- src/components/Modals/SeaBattle/service/sea-battle.service.ts
+++ src/components/Modals/SeaBattle/service/sea-battle.service.ts
@@ -23,23 +23,17 @@ export async function fetchStats() {
 }
 
 export async function fetchUserPhoto(username: string) {
-  const url = `https://t.me/i/userpic/160/${username}.jpg`;
+  const url = `https://socket.akronix.io/main/getTgImageLink?username=${username}`;
 
   try {
-    const res = await fetch(url, { redirect: "follow" });
-    console.log({ resAva: res });
-
-    if (res.status === 302) {
-      const location = res.headers.get("Location");
-      if (location) {
-        return location;
-      }
-    } else if (res.ok) {
-      console.log({ resOkAva: res });
-      return url;
+    const res = await fetch(url);
+    // @ts-ignore
+    if (res?.image) {
+      // @ts-ignore
+      return res?.image;
+    } else {
+      return null;
     }
-
-    return null;
   } catch (e) {
     return null;
   }
